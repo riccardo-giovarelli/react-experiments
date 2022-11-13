026ef560b0f9124b4194920dc062181f04f341d7
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2022-11-13 23:35:20 +0100

    chore: some tuning

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -12,6 +12,9 @@ const AccordionItem = ({ data, defaultStatus = false }: AccordionItemPropsType) 
           "accordionitem__header py-5 px-3 flex justify-between border-2 border-slate-600 justify-between accordionitem__header " +
           (isOpen ? "bg-slate-600" : "bg-slate-100")
         }
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
       >
         <div
           className={
@@ -21,20 +24,12 @@ const AccordionItem = ({ data, defaultStatus = false }: AccordionItemPropsType) 
         >
           {data.title}
         </div>
-        <div
-          className={
-            "accordionitem__header-button-container accordionitem__header-button-container" +
-            (isOpen ? "--open" : "--close")
-          }
-        >
+        <div className="accordionitem__header-button-container accordionitem__header-button-container">
           <span
             className={
-              "accordionitem__header-button material-symbols-outlined grow-0 " +
-              (isOpen ? "text-slate-100" : "text-slate-600")
+              "accordionitem__header-button material-symbols-outlined grow-0 text-xl accordionitem__header-button" +
+              (isOpen ? "--open text-slate-100" : "--close text-slate-600")
             }
-            onClick={() => {
-              setIsOpen(!isOpen);
-            }}
           >
             arrow_downward
           </span>
--- src/pages/page-accordion/PageAccordion.tsx
+++ src/pages/page-accordion/PageAccordion.tsx
@@ -1,12 +1,10 @@
 import Accordion from "../../components/accordion/Accordion";
 import { getAccordionItems } from "../../model/data-provider";
 
-const PageAccordion = () => {
-  return (
-    <div className="page-accordion__container w-full flex justify-center">
-      <Accordion accordionItems={getAccordionItems()} />
-    </div>
-  );
-};
+const PageAccordion = () => (
+  <div className="page-accordion__container w-full flex justify-center">
+    <Accordion accordionItems={getAccordionItems()} />
+  </div>
+);
 
 export default PageAccordion;
